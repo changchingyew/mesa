c0e0e4a26671b74dc3445f969a4e3e3572c0a85e
Sil Vilerino <sivileri@microsoft.com>
2022-01-26 15:39:14 -0800

    Style formatting for src/gallium/drivers/d3d12/d3d12_video_encoder_bitstream.h

--- src/gallium/drivers/d3d12/d3d12_video_encoder_bitstream.h
+++ src/gallium/drivers/d3d12/d3d12_video_encoder_bitstream.h
@@ -26,102 +26,102 @@
 
 #include "d3d12_video_types.h"
 
-#define DELETE_ARRAY(ptr)       {if (ptr) {delete [] (ptr); (ptr) = NULL;}}
+#define DELETE_ARRAY(ptr)                                                                                              \
+   {                                                                                                                   \
+      if (ptr) {                                                                                                       \
+         delete[](ptr);                                                                                                \
+         (ptr) = NULL;                                                                                                 \
+      }                                                                                                                \
+   }
 
 class D3D12VideoBitstream
 {
-public:
-    D3D12VideoBitstream ();
-    ~D3D12VideoBitstream ();
+ public:
+   D3D12VideoBitstream();
+   ~D3D12VideoBitstream();
 
-public:
-    void  GetCurrentBufferPostionAndSize(uint8_t  **ppCurrBufPos, int32_t *pdwLeftBufSize );
-    void  IncCurrentOffset(int32_t dwOffset );
-    bool CreateBitStream (uint32_t uiInitBufferSize);
-    void SetupBitStream( uint32_t uiInitBufferSize, uint8_t *pBuffer );
-    void Attach (uint8_t *pBitsBuffer, uint32_t uiBufferSize);
-    void PutBits (int32_t uiBitsCount, uint32_t iBitsVal);
-    void Flush ();
-    void Exp_Golomb_ue (uint32_t uiVal);
-    void Exp_Golomb_se (int32_t iVal);
+ public:
+   void GetCurrentBufferPostionAndSize(uint8_t **ppCurrBufPos, int32_t *pdwLeftBufSize);
+   void IncCurrentOffset(int32_t dwOffset);
+   bool CreateBitStream(uint32_t uiInitBufferSize);
+   void SetupBitStream(uint32_t uiInitBufferSize, uint8_t *pBuffer);
+   void Attach(uint8_t *pBitsBuffer, uint32_t uiBufferSize);
+   void PutBits(int32_t uiBitsCount, uint32_t iBitsVal);
+   void Flush();
+   void Exp_Golomb_ue(uint32_t uiVal);
+   void Exp_Golomb_se(int32_t iVal);
 
-    inline void Clear ()
-    {
-        m_iBitsToGo = 32;
-        m_uiOffset = 0;
-        m_uintEncBuffer = 0;
-    };
+   inline void Clear()
+   {
+      m_iBitsToGo     = 32;
+      m_uiOffset      = 0;
+      m_uintEncBuffer = 0;
+   };
 
-    void AppendByteStream (D3D12VideoBitstream *pStream);
+   void AppendByteStream(D3D12VideoBitstream *pStream);
 
-    void SetStartCodePrevention (bool bSCP)
-    {
-        m_bPreventStartCode = bSCP;
-    }
-    int32_t  GetBitsCount ()
-    {
-        return m_uiOffset * 8 + (32 - m_iBitsToGo);
-    }
-    int32_t  GetByteCount ()
-    {
-        return m_uiOffset + ((32 - m_iBitsToGo) >> 3);
-    }
-    uint8_t *GetBitstreamBuffer ()
-    {
-        return m_pBitsBuffer;
-    }
-    bool IsByteAligned ()
-    {
-        if (m_bBufferOverflow)
-        {
-            m_iBitsToGo = 32;
-        }
-        return !(m_iBitsToGo & 7);
-    }
-    int32_t  GetNumBitsForByteAlign ()
-    {
-        return (m_iBitsToGo & 7);
-    }
-    bool GetStartCodePreventionStatus ()
-    {
-        return m_bPreventStartCode;
-    }
-    bool VerifyBuffer(uint32_t uiBytesToWrite);
+   void SetStartCodePrevention(bool bSCP)
+   {
+      m_bPreventStartCode = bSCP;
+   }
+   int32_t GetBitsCount()
+   {
+      return m_uiOffset * 8 + (32 - m_iBitsToGo);
+   }
+   int32_t GetByteCount()
+   {
+      return m_uiOffset + ((32 - m_iBitsToGo) >> 3);
+   }
+   uint8_t *GetBitstreamBuffer()
+   {
+      return m_pBitsBuffer;
+   }
+   bool IsByteAligned()
+   {
+      if (m_bBufferOverflow) {
+         m_iBitsToGo = 32;
+      }
+      return !(m_iBitsToGo & 7);
+   }
+   int32_t GetNumBitsForByteAlign()
+   {
+      return (m_iBitsToGo & 7);
+   }
+   bool GetStartCodePreventionStatus()
+   {
+      return m_bPreventStartCode;
+   }
+   bool VerifyBuffer(uint32_t uiBytesToWrite);
 
-public:
-    bool m_bBufferOverflow;
-    bool m_bAllowReallocate;
+ public:
+   bool m_bBufferOverflow;
+   bool m_bAllowReallocate;
 
-private:
-    void WriteByteStartCodePrevention (uint8_t u8Val);
-    bool ReallocateBuffer ();
-    int32_t GetExpGolomb0CodeLength (uint32_t uiVal);
+ private:
+   void    WriteByteStartCodePrevention(uint8_t u8Val);
+   bool    ReallocateBuffer();
+   int32_t GetExpGolomb0CodeLength(uint32_t uiVal);
 
-    const uint8_t m_iLog_2_N[256] =
-    {
-        0, 0, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4,
-        4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
-        5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6,
-        6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
-        6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
-        6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
-        7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
-        7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
-        7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
-        7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
-        7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7
-    };
+   const uint8_t m_iLog_2_N[256] = {
+      0, 0, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5,
+      5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+      6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+      6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+      7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+      7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+      7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7
+   };
 
-private:
-    uint8_t  *m_pBitsBuffer;
-    uint32_t m_uiBitsBufferSize;
-    uint32_t  m_uiOffset;
+ private:
+   uint8_t *m_pBitsBuffer;
+   uint32_t m_uiBitsBufferSize;
+   uint32_t m_uiOffset;
 
-    bool m_bExternalBuffer;
-    uint32_t m_uintEncBuffer;
-    int32_t  m_iBitsToGo;
+   bool     m_bExternalBuffer;
+   uint32_t m_uintEncBuffer;
+   int32_t  m_iBitsToGo;
 
-    bool m_bPreventStartCode;
+   bool m_bPreventStartCode;
 };
 
 #endif
