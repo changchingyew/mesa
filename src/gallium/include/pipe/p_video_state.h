06e4860876ea28a0dc67d0023e460f6eb80ebc68
Sil Vilerino <sivileri@microsoft.com>
2022-04-27 09:46:27 -0700

    Gallium: Use ref_idx_lx_list instead of union

--- src/gallium/include/pipe/p_video_state.h
+++ src/gallium/include/pipe/p_video_state.h
@@ -423,14 +423,8 @@ struct pipe_h264_enc_picture_desc
    unsigned pic_order_cnt_type;
    unsigned num_ref_idx_l0_active_minus1;
    unsigned num_ref_idx_l1_active_minus1;
-   union {
-      unsigned ref_idx_l0;
-      unsigned ref_idx_l0_list[32];
-   };
-   union {   
-      unsigned ref_idx_l1;
-      unsigned ref_idx_l1_list[32];
-   };
+   unsigned ref_idx_l0_list[32];
+   unsigned ref_idx_l1_list[32];
    unsigned gop_size;
    unsigned ref_pic_mode;
    unsigned num_temporal_layers;
